263e783ff5eb1a7ffdc9d2e6357a4348b877f71e
Suki Huang <tony13578@gmail.com>
2017-02-15 18:40:00 +0800

    android_toolkit_utils

---
+++ android_toolkit_utils/app/src/main/java/com/voicetube/toolkit/badger/ShortcutBadgeException.java
@@ -0,0 +1,8 @@
+package com.voicetube.toolkit.badger;
+
+@Deprecated
+public class ShortcutBadgeException extends Exception {
+    public ShortcutBadgeException(String message) {
+        super(message);
+    }
+}
---
+++ android_toolkit_utils/app/src/main/java/com/voicetube/toolkit/badger/util/CloseHelper.java
@@ -0,0 +1,14 @@
+package com.voicetube.toolkit.badger.util;
+
+import android.database.Cursor;
+
+/**
+ * @author leolin
+ */
+public class CloseHelper {
+    public static void close(Cursor cursor) {
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+    }
+}
